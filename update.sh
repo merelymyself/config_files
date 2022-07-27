6036ea496798e34c468be5782def0d2b263b81be
merelymyself <poonsters6@gmail.com>
2022-07-27 17:02:32 +0800

    Date is Wed Jul 27 05:02:32 PM +08 2022

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp ~/.config/fish/config.fish ./fish
+cp ~/.config/fish/config.fish ./config/fish
 
-cp -r ~/.config/macchina/ .
+cp -r ~/.config/macchina/ ./config
 
-cp ~/.config/alacritty/alacritty.yml ./alacritty
+cp ~/.config/alacritty/alacritty.yml ./config/alacritty
 
-cp ~/.config/nvim/init.vim ./neovim
+cp ~/.config/nvim/init.vim ./config/neovim
 
 git add --all
 
