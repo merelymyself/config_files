140dad023970ea8ab101c2ebdcbf83f057558913
merelymyself <poonsters6@gmail.com>
2022-10-19 20:14:39 +0800

    Date is: Wed Oct 19 08:14:39 PM +08 2022

--- .config/i3blocks/wifi.sh
+++ .config/i3blocks/wifi.sh
@@ -3,4 +3,11 @@
 id=$(ifconfig | awk '/^wl/ { print $1 }' | sed 's/.$//')
 ssid=$(nmcli connection show --active | awk 'NF-=3' | awk 'NR>1')
 quality=$(iwconfig $id | grep quality -i | awk '{ print $2 }' | cut -c 9-)
-echo "ᯤ" $ssid "||" $quality
+
+if ifconfig | grep -q "proton0"; then
+	echo -n "🗝️";
+else
+	echo -n "ᯤ "; 
+fi;
+echo -n $ssid 
+echo -n ";" $quality
